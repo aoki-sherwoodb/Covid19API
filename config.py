4e5df0bc2e6005f013dd272652733663b5eb9a01
Ben Aoki-Sherwood <63218319+aoki-sherwoodb@users.noreply.github.com>
2020-12-08 18:33:17 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+#config settings for psycopg2 access to the database. Assign values to the variables below according to how the db is stored on your setup.
+
+database = 'database_name'
+user = 'username'
+password = 'password'
